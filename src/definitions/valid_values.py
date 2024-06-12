07a1350833e28ee33e42a8cd29b7cd97352fae8a
Patrick Michiels <it@patrickmichiels.de>
2024-06-12 10:39:19 +0200

    valid values: removed remnants of autoscaling

--- src/definitions/valid_values.py
+++ src/definitions/valid_values.py
@@ -1,17 +1,5 @@
 from enum import Enum
 
-class ScalingConflictResolution(Enum):
-    SCALE_UP = "scale_up"
-    SCALE_DOWN = "scale_down"
-    KEEP_REPLICAS = "keep_replicas"
-    ADHERE_TO_MEMORY = "adhere_to_memory"
-    ADHERE_TO_CPU = "adhere_to_cpu"
-
-class ScalingSuggestion(Enum):
-    SCALE_UP = "scale_up"
-    SCALE_DOWN = "scale_down"
-    KEEP_REPLICAS = "keep_replicas"
-
 class LogLevel(Enum):
     INFO = "INFO"
     VERBOSE = "VERBOSE"
@@ -33,10 +21,6 @@ class MessageLevel(Enum):
     
     def get_lowercase_representation_with_icon(self):
         return f'{self.icon} {str(self.level).lower()}'
-
-class ScalingMetricName(Enum):
-    CPU = "CPU"
-    MEMORY = "MEMORY"
 
 class MessagingPlatforms(Enum):
     LOGGING = "LOGGING"
